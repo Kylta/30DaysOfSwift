bce1930e8ef46f0bf2c01b1dff4bfea5b21f9aba
Kylta <Christophe.bugnon02@gmail.com>
2018-02-26 10:51:08 +0100

    create Project 01 - SimpleStopWatch

---
+++ Project 01 - SimpleStopWatch/Project 01 - SimpleStopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 01 - SimpleStopWatch
+//
+//  Created by Christophe Bugnon on 26/02/2018.
+//  Copyright © 2018 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
